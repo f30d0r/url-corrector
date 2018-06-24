0117ceb64c3ed8e0863eacf50afe067f57e0190f
f30d0r <f.rusanov@gmail.com>
2018-06-24 11:10:08 +0300

    Fixed spelling mistake

--- url_corrector/lang/ru_RU.php
+++ url_corrector/lang/ru_RU.php
@@ -1,5 +1,5 @@
 <?php
 $i18n = array(
-  "PLUGIN_DESCRIPTION" => "Корректирует непраильные URL.",
+  "PLUGIN_DESCRIPTION" => "Корректирует неправильные URL.",
 	"PRETTYURLS_OFF" => "ЧПУ отклчены. <b>URL Corrector</b> не работает!"
 );
